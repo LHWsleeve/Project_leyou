1e3dc7a331e2b68d5adfe8d29a54b621ce7af202
sleeve <1421147715@qq.com>
2020-01-31 23:58:49 +0800

    读取资源配置的类
    使用application.properties///ConfigurationProperties

--- code/demo/itcat_sringboot/src/main/java/cn/itcast/springboot/config/JdbcConfig.java
+++ code/demo/itcat_sringboot/src/main/java/cn/itcast/springboot/config/JdbcConfig.java
@@ -1,7 +1,9 @@
 package cn.itcast.springboot.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -9,24 +11,19 @@ import org.springframework.context.annotation.PropertySource;
 import javax.sql.DataSource;
 
 @Configuration//声明一个类是一个java配置类，相当于XML配置
-@PropertySource("classpath:jdbc.properties")//读取外部资源文件
+//@PropertySource("classpath:jdbc.properties")//读取外部资源文件
+@EnableConfigurationProperties(JdbcProperties.class)//起用资源配置读取类
 public class JdbcConfig {
-    @Value("${jdbc.driverClassName}")//获取spring容器中的资源文件配置
-    private String driverClassName;
-    @Value("${jdbc.url}")
-    private String url;
-    @Value("${jdbc.username}")
-    private String  username;
-    @Value("${jdbc.password}")
-    private String  password;
+    @Autowired
+    private JdbcProperties jdbcProperties;
 
     @Bean//把方法的返回值注入spring容器
     public DataSource dataSource(){
         DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setDriverClassName(this.driverClassName);
-        dataSource.setUrl(this.url);
-        dataSource.setUrl(this.username);
-        dataSource.setUrl(this.password);
+        dataSource.setDriverClassName(this.jdbcProperties.getDriverClassName());
+        dataSource.setUrl(this.jdbcProperties.getUrl());
+        dataSource.setUrl(this.jdbcProperties.getUsername());
+        dataSource.setUrl(this.jdbcProperties.getPassword());
         return dataSource;
     }
 }
