be2313fbc573c2da35e2422898b56d872c27f02f
sleeve <1421147715@qq.com>
2020-02-01 19:12:15 +0800

    整合SpringMVC
    1.静态文件保存在resources/static中（这是一级目录）
    2.添加拦截器。使用实现WebMvcConfigurer接口的方式

---
+++ code/demo/itcast-user/src/main/java/cn/itcast/user/config/MvcWebConfig.java
@@ -0,0 +1,18 @@
+package cn.itcast.user.config;
+
+import cn.itcast.user.interceptor.MyInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class MvcWebConfig implements WebMvcConfigurer {
+    @Autowired
+    private MyInterceptor myInterceptor;
+    //添加拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(myInterceptor).addPathPatterns("/**");
+    }
+}
