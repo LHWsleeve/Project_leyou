cd886aba15be88aaa6b83e99e3ee7096d51637f0
sleeve <1421147715@qq.com>
2020-02-01 14:07:35 +0800

    四种属性注入
    1.autowired最常见，最多用
    2.使用构造方法传入形参
    3.直接在Bean的构造方法中歘纳入相应形参
    4.直接再Bean中注入，全部自动解析，不需要资源读取类。但是局限性太大

--- code/demo/itcat_sringboot/src/main/java/cn/itcast/springboot/config/JdbcConfig.java
+++ code/demo/itcat_sringboot/src/main/java/cn/itcast/springboot/config/JdbcConfig.java
@@ -14,16 +14,23 @@ import javax.sql.DataSource;
 //@PropertySource("classpath:jdbc.properties")//读取外部资源文件
 @EnableConfigurationProperties(JdbcProperties.class)//起用资源配置读取类
 public class JdbcConfig {
-    @Autowired
-    private JdbcProperties jdbcProperties;
+//    @Autowired //使用自动注入，属性
+//    private JdbcProperties jdbcProperties;
 
-    @Bean//把方法的返回值注入spring容器
-    public DataSource dataSource(){
+    //使用构造方法属性注入
+//    public JdbcConfig(JdbcProperties jdbcProperties) {
+////        this.jdbcProperties=jdbcProperties;
+////    }
+
+
+
+    @Bean//把方法的返回值注入spring容器，也可以在这里通过方法的形参注入数据库属性
+    public DataSource dataSource(JdbcProperties jdbcProperties){
         DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setDriverClassName(this.jdbcProperties.getDriverClassName());
-        dataSource.setUrl(this.jdbcProperties.getUrl());
-        dataSource.setUrl(this.jdbcProperties.getUsername());
-        dataSource.setUrl(this.jdbcProperties.getPassword());
+        dataSource.setDriverClassName(jdbcProperties.getDriverClassName());
+        dataSource.setUrl(jdbcProperties.getUrl());
+        dataSource.setUrl(jdbcProperties.getUsername());
+        dataSource.setUrl(jdbcProperties.getPassword());
         return dataSource;
     }
 }
