dca96283f79058f0b14b114fd5f911788b516c0c
sleeve <1421147715@qq.com>
2020-02-02 18:43:46 +0800

    thymeleaf

--- code/demo/itcast-user/src/main/java/cn/itcast/user/controller/UserController.java
+++ code/demo/itcast-user/src/main/java/cn/itcast/user/controller/UserController.java
@@ -4,10 +4,13 @@ import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -25,5 +28,12 @@ public class UserController {
     @ResponseBody
     public User queryById(@PathVariable("id") Integer id){
         return this.userService.querUserById(id);
+    }
+
+    @GetMapping("all")
+    public String queryUserAll(Model model){
+        List<User> userList = this.userService.queryUserAll();
+        model.addAttribute("users",userList);
+        return "users";
     }
 }
