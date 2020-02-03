fba3eac626f9d558ff43d6ca1e8619a621c2dff7
sleeve <1421147715@qq.com>
2020-02-03 18:06:01 +0800

    eureka的使用
    将消费模块和数据提供模块注册到eureka中。
    再消费者某块中使用restTemplate调用

--- code/demo/itcast-service-consumer/src/main/java/cn/itcast/service/controller/UserController.java
+++ code/demo/itcast-service-consumer/src/main/java/cn/itcast/service/controller/UserController.java
@@ -1,20 +1,32 @@
 package cn.itcast.service.controller;
 
 import cn.itcast.service.pojo.User;
+import org.omg.PortableInterceptor.INACTIVE;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RequestMapping("/consumer/user")
 @RestController
 public class UserController {
     @Autowired
     private RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping
-    public User querUserById(){
-       return this.restTemplate.getForObject("http://localhost:10001/user/1",User.class);
-     }
+    public User querUserById(@RequestParam("id")Integer id){
+        //通过服务的id获取服务的集合（实际工作中可能是集群，所以返回集合）
+        List<ServiceInstance> instances = discoveryClient.getInstances("SERVICE-PROVIDER");
+        ServiceInstance serviceInstance = instances.get(0);
+        return this.restTemplate.getForObject("http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+"/user/"+id,User.class);
+    }
+
 }
--- code/demo/itcast-service-provider/src/main/java/cn/itcast/service/ItcastServiceProviderApplication.java
+++ code/demo/itcast-service-provider/src/main/java/cn/itcast/service/ItcastServiceProviderApplication.java
@@ -2,8 +2,10 @@ package cn.itcast.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
+@EnableDiscoveryClient//开启erueka的客户端，这注解由spring提供，封装了奈飞的erueka客户端@EnableEurekaClient
 @SpringBootApplication
 @MapperScan("cn.itcast.service.mapper")//开启mapper接口扫描。不需要再mapper上加@mapper注解
 public class ItcastServiceProviderApplication {
