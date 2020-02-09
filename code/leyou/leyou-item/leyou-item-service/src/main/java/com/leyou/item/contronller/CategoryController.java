bf761d8837469a9d8e5784c4cab6096e1ed9951c
sleeve <1421147715@qq.com>
2020-02-09 16:28:40 +0800

    分类查询
    存在跨域问题CORS

---
+++ code/leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/contronller/CategoryController.java
@@ -0,0 +1,46 @@
+package com.leyou.item.contronller;
+
+import com.leyou.item.pojo.Category;
+import com.leyou.item.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 根据父类目的id查询所有子节点
+     * @param pid
+     * @return
+     */
+    @GetMapping("/list")
+    public ResponseEntity<List<Category>> queryCategorysByPid(@RequestParam(value = "pid", defaultValue = "0") Long pid) {
+        if (pid == null || pid < 0) {
+            //参数不合法相应400,三种写法
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+//                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntity.badRequest().build();
+        }
+
+        //执行查询获取结果集
+        List<Category> categories = this.categoryService.queryCategoryByPid(pid);
+        //判断查询结果是否为空，若为空相应404
+        if (CollectionUtils.isEmpty(categories)) {
+//                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        //正常找到相应200
+        return ResponseEntity.ok(categories);
+    }
+}
---
+++ code/leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -0,0 +1,25 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.CategroyMapper;
+import com.leyou.item.pojo.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategroyMapper categroyMapper;
+
+    /**
+     * 根据父id查询子类目
+     * @param pid
+     * @return
+     */
+    public List<Category> queryCategoryByPid(Long pid) {
+        Category record = new Category();
+        record.setParentId(pid);
+        return this.categroyMapper.select(record);
+    }
+}
