e5b169a2a3b87a56d298d10a4664bea6ab5e7e7b
sleeve <1421147715@qq.com>
2020-02-19 16:24:56 +0800

    高级查询
    1.        //构建一个自定义查询构建器
    2.        //构建起添加查询条件
    3.        //构建器添加分页条件。！！！！页码是从0开始的，指定2，实际上是数据库中的第三页
    4.        //添加排序条件
     5.       //执行查询（因为是构建器，所以需要先build()），返回分页结果集

--- code/demo/itcast-elasticsearch/src/test/java/cn/itcast/elasticsearch/ItcastElasticsearchApplicationTests.java
+++ code/demo/itcast-elasticsearch/src/test/java/cn/itcast/elasticsearch/ItcastElasticsearchApplicationTests.java
@@ -2,12 +2,21 @@ package cn.itcast.elasticsearch;
 
 import cn.itcast.elasticsearch.pojo.Item;
 import cn.itcast.elasticsearch.repository.ItemRepository;
+import org.elasticsearch.index.query.MatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -40,6 +49,12 @@ public class ItcastElasticsearchApplicationTests {
         List<Item> list = new ArrayList<>();
         list.add(new Item(2L, "坚果手机R1", " 手机", "锤子", 3699.00, "http://image.leyou.com/123.jpg"));
         list.add(new Item(3L, "华为META10", " 手机", "华为", 4499.00, "http://image.leyou.com/3.jpg"));
+        list.add(new Item(1L, "小米手机7", "手机", "小米", 3299.00, "http://image.leyou.com/13123.jpg"));
+        list.add(new Item(4L, "嘿嘿手机R1", "手机", "锤子", 3699.00, "http://image.leyou.com/13123.jpg"));
+        list.add(new Item(5L, "哈哈META10", "手机", "华为", 4499.00, "http://image.leyou.com/13123.jpg"));
+        list.add(new Item(6L, "小米Mix2S", "手机", "小米", 4299.00, "http://image.leyou.com/13123.jpg"));
+        list.add(new Item(7L, "荣耀V10", "手机", "华为", 2799.00, "http://image.leyou.com/13123.jpg"));
+
         // 接收对象集合，实现批量新增
         itemRepository.saveAll(list);
     }
@@ -52,4 +67,38 @@ public class ItcastElasticsearchApplicationTests {
         List<Item> items = this.itemRepository.findByPriceBetween(3499d, 3699d);
         items.forEach(System.out::println);//方法引用
     }
+
+    //高级查询
+    @Test
+    public void testMacth(){
+        MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("title", "手机");
+        Iterable<Item> items = this.itemRepository.search(matchQueryBuilder);
+        items.forEach(System.out::println);
+    }
+    //自定义查询
+    @Test
+    public void testNative(){
+        //构建一个自定义查询构建器
+        NativeSearchQueryBuilder queryBuilder = new NativeSearchQueryBuilder();
+        //构建起添加查询条件
+        queryBuilder.withQuery(QueryBuilders.matchQuery("category", "手机"));
+        //构建器添加分页条件。！！！！页码是从0开始的，指定2，实际上是数据库中的第三页
+        queryBuilder.withPageable(PageRequest.of(0,5));
+        //添加排序条件
+        queryBuilder.withSort(SortBuilders.fieldSort("price").order(SortOrder.ASC));
+        //执行查询（因为是构建器，所以需要先build()），返回分页结果集
+        Page<Item> itemPage = this.itemRepository.search(queryBuilder.build());
+        //总页数
+        System.out.println(itemPage.getTotalPages());
+        //总条数
+        System.out.println(itemPage.getTotalElements());
+        //当前页的记录
+        itemPage.forEach(System.out::println);
+    }
+
+
+
+
+
+
 }
