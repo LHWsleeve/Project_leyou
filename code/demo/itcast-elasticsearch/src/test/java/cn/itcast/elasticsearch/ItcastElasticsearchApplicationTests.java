855adbc1e12920b09c6eb47bfb787c36ec8f543b
sleeve <1421147715@qq.com>
2020-02-19 14:41:38 +0800

    在测试文件中，创建索引库和映射关系

--- code/demo/itcast-elasticsearch/src/test/java/cn/itcast/elasticsearch/ItcastElasticsearchApplicationTests.java
+++ code/demo/itcast-elasticsearch/src/test/java/cn/itcast/elasticsearch/ItcastElasticsearchApplicationTests.java
@@ -1,13 +1,23 @@
 package cn.itcast.elasticsearch;
 
-import org.junit.jupiter.api.Test;
+import cn.itcast.elasticsearch.pojo.Item;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class ItcastElasticsearchApplicationTests {
+@RunWith(SpringRunner.class)
+public class ItcastElasticsearchApplicationTests {
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
 
     @Test
-    void contextLoads() {
+   public void testIndex() {
+        this.elasticsearchTemplate.createIndex(Item.class);//基于pojo注解创建索引库
+        this.elasticsearchTemplate.putMapping(Item.class);//创建映射
     }
 
 }
